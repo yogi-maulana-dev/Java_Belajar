b4343ef991ea2c85198a7b8785f775ce8b78d349
yogi-maulana-dev <yogimaulana100@gmail.com>
2026-05-13 20:03:09 +0700

    belajar Tipe Data Char

---
+++ Menampilkan Output/src/TipeDataChar.java
@@ -0,0 +1,8 @@
+public class TipeDataChar{
+    public static void main(String[] args) {
+        char e = 'E';
+        char k = 'K';
+        char o = 'O';
+        System.out.println(e);
+    }
+}
